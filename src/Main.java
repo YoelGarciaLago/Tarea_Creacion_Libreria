af37ec610dbef6d88c98e99551d0697b3b046ead
Yoel <yoelgarcialago@gmail.com>
2024-03-17 13:29:44 +0100

    entrada y salida

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import com.damLibrerias.Entrada;
+import com.damLibrerias.Salida;
+public class Main {
+    public static void main(String[] args) {
+        float [] lista = Entrada.elegirNúmeros();
+        float n1 = lista[0], n2 = lista [1];
+    }
+}
