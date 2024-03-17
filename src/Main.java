94401c87868f721c21c32c46af38702dde73e761
Yoel <yoelgarcialago@gmail.com>
2024-03-17 13:33:53 +0100

    cacl añadido

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import com.damLibrerias.Entrada;
 import com.damLibrerias.Salida;
+import com.damLibrerias.Calc;
 public class Main {
     public static void main(String[] args) {
         float [] lista = Entrada.elegirNúmeros();
         float n1 = lista[0], n2 = lista [1];
+        System.out.println(Calc.Calc(n1,n2,1));
+
     }
 }
